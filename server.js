86c763db2b355f8e5fe3d650d6dccde9a9301d77
AKSHAY KUMAR VERMA <akshaykumarnitj@gmail.com>
2021-04-22 21:17:37 +0530

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express()
 
 app.use('/',express.static(__dirname+'/public'))
 
-const PORT=process.env.PORT|4444
+const PORT=process.env.PORT||4444;
 
 app.listen(PORT,()=>{
     console.log(`server host on http://localhost:${PORT}`)
